4f9c02d2847b219d996f88675074645a2ae650f6
nooblue <null>
2024-05-25 08:32:14 +0300

    8.11 8.12 authors birth year can be changed

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,5 +1,45 @@
-import { useQuery } from "@apollo/client";
-import { ALL_AUTHORS } from "../queries";
+import { useMutation, useQuery } from "@apollo/client";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
+import { useState } from "react";
+
+const AuthorBirthYear = ({ authors }) => {
+  const [author, setAuthor] = useState("");
+  const [year, setYear] = useState("");
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  const selectEvent = (e) => {
+    e.preventDefault();
+    setAuthor(e.target.value);
+  };
+
+  const yearEvent = (e) => {
+    e.preventDefault();
+    setYear(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    editAuthor({ variables: { name: author, born: parseInt(year) } });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2>Edit author</h2>
+      <select value={author} onChange={selectEvent}>
+        {authors.map((a) => (
+          <option key={a.name} value={a.name}>
+            {a.name}
+          </option>
+        ))}
+      </select>
+      <input type="number" value={year} onChange={yearEvent}></input>
+      <button type="submit">Update author</button>
+    </form>
+  );
+};
 
 const Authors = (props) => {
   const authorsResult = useQuery(ALL_AUTHORS);
@@ -31,6 +71,7 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <AuthorBirthYear authors={authors}></AuthorBirthYear>
     </div>
   );
 };
